0c3076921ddd8580aa5b22fb74d5d874fde7c0fd
Sandy Yeung <sandynism@gmail.com>
2017-08-30 00:32:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,30 @@
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number) // 3.333333333335
+
+add5()
+
+console.log(number) // 8.333333333335
+
+// reset number
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
+parseInt('2.2222', 10)
